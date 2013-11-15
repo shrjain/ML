053d9a51526113107b63e6429ca694e8f2d6d694
shrjain <shrainik@gmail.com>
2013-11-14 18:46:57 -0800

    HW3
    base commit

---
+++ HW3/HW3.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 14 18:39:20 2013
+
+@author: shrainik
+"""
+import numpy as np
+
+if __name__ == '__main__':
+    path_prefix = '/Users/shrainik/ML/HW3'
+    y = np.loadtxt("", dtype=np.int)
